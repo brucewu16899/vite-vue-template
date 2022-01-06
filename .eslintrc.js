e25bcfa8fa300d6e07287550283002abcb4443b4
shafucheng <flynn.sha@outlook.com>
2022-01-07 01:39:03 +0800

    add unplugin-auto-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,7 @@ module.exports = {
       jsx: true,
     },
   },
-  rules: {},
+  rules: {
+    'no-undef': 'off',
+  },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from '@nabla/vite-plugin-eslint'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,10 @@ export default defineConfig({
     vue(),
     eslint({
       shouldLint: (path) => /\/.*\.([jt]sx?|vue)$/.test(path),
+    }),
+    AutoImport({
+      imports: ['vue'],
+      dts: 'src/auto-imports.d.ts',
     }),
   ],
 })
