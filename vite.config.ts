692cd0a15d52ed79e4176e9b4c713680df9909df
shafucheng <flynn.sha@outlook.com>
2022-01-06 20:23:18 +0800

    add @nabla/vite-plugin-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from '@nabla/vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslint({
+      shouldLint: (path) => /\/.*\.([jt]sx?|vue)$/.test(path),
+    }),
+  ],
 })
