8eaa51a208430440e4d63c149dc35c32c6ebc929
shafucheng <flynn.sha@outlook.com>
2022-01-07 01:48:34 +0800

    add unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from '@nabla/vite-plugin-eslint'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,9 @@ export default defineConfig({
     AutoImport({
       imports: ['vue'],
       dts: 'src/auto-imports.d.ts',
+    }),
+    Components({
+      dts: 'src/components.d.ts',
     }),
   ],
 })
