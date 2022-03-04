b03459c74df123495b76d8ed9a81b2c7eeb63cc0
shafucheng <flynn.sha@outlook.com>
2022-03-04 22:08:32 +0800

    chore: 使用 unplugin-auto-import 自动导入库

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import * as path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import checker from 'vite-plugin-checker'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   plugins: [
@@ -13,6 +14,10 @@ export default defineConfig({
       eslint: {
         lintCommand: 'eslint ./**/*.{json,js,jsx,ts,tsx,vue}',
       },
+    }),
+    AutoImport({
+      imports: ['vue'],
+      dts: 'src/auto-imports.d.ts',
     }),
   ],
   resolve: {
