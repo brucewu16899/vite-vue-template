7a3da455ca6b54c4f7dfe76a0571d1b3c9000164
shafucheng <flynn.sha@outlook.com>
2022-03-04 20:21:06 +0800

    chore: 添加 ESLint+Prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
   plugins: [
     vue({
       reactivityTransform: true,
+    }),
+    checker({
+      vueTsc: true,
+      eslint: {
+        lintCommand: 'eslint ./**/*.{json,js,jsx,ts,tsx,vue}',
+      },
     }),
   ],
 })
