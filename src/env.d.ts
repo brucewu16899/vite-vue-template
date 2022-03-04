7f4d24f24c3b221580e3d6468e40efcb587fcaba
shafucheng <flynn.sha@outlook.com>
2022-03-05 01:03:41 +0800

    chore: 使用 vite-plugin-html

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vue/macros-global" />
 
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+}
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,50 @@
 import * as path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import vue from '@vitejs/plugin-vue'
 import eslint from '@nabla/vite-plugin-eslint'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 
-export default defineConfig({
-  plugins: [
-    vue({
-      reactivityTransform: true,
-    }),
-    eslint({
-      shouldLint: (path) => /\/.*\.([jt]sx?|vue)$/.test(path),
-      eslintOptions: {
-        fix: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname)
+
+  return {
+    plugins: [
+      createHtmlPlugin({
+        inject: {
+          data: {
+            ...env,
+          },
+        },
+        minify: true,
+      }),
+      vue({
+        reactivityTransform: true,
+      }),
+      eslint({
+        shouldLint: (path) => /\/.*\.([jt]sx?|vue)$/.test(path),
+        eslintOptions: {
+          fix: true,
+        },
+      }),
+      AutoImport({
+        imports: ['vue', 'vue-router', '@vueuse/core'],
+        eslintrc: {
+          enabled: true,
+          filepath: './.eslintrc-auto-import.json',
+          globalsPropValue: true,
+        },
+        dts: 'src/auto-imports.d.ts',
+      }),
+      Components({
+        dts: 'src/components.d.ts',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, 'src'),
       },
-    }),
-    AutoImport({
-      imports: ['vue', 'vue-router', '@vueuse/core'],
-      eslintrc: {
-        enabled: true,
-        filepath: './.eslintrc-auto-import.json',
-        globalsPropValue: true,
-      },
-      dts: 'src/auto-imports.d.ts',
-    }),
-    Components({
-      dts: 'src/components.d.ts',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, 'src'),
     },
-  },
+  }
 })
