1cf2e83b23a4070a54f2958555afa570f065c7b8
shafucheng <flynn.sha@outlook.com>
2022-03-04 23:15:19 +0800

    chore: 添加依赖 vue-router

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       },
     }),
     AutoImport({
-      imports: ['vue', '@vueuse/core'],
+      imports: ['vue', 'vue-router', '@vueuse/core'],
       eslintrc: {
         enabled: true,
         filepath: './.eslintrc-auto-import.json',
